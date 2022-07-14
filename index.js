d13e80891fc20911abcdbb6b79b2d4fdda7e368e
haindn <haindn999@gmail.com>
2022-07-14 20:15:11 +0700

    hai change route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-app.get('/contact', function (req, ress) {
-  res.send('Hello node');
+app.get('/route', function (req, ress) {
+  res.send('Hello Nguyen Hai');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
