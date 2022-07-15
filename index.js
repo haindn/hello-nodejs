e24fa3b32f9e32ac2ba5355f2862d7d6fd91a66a
haindn <109093636+haindn@users.noreply.github.com>
2022-07-15 08:56:49 +0700

    Update index.js
    Change index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/route', function (req, ress) {
-  res.send('Hello Nguyen Hai');
+  res.send('Hello Nguyen Hai Ngoc');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
